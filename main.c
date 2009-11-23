b4dd6fef02fc33a73efb1608a6cff56f9744c5a1
Ryan Hope <rmh3093@gmail.com>
2009-11-22 22:09:21 -0500

    more progress

--- main.c
+++ main.c
@@ -16,54 +16,135 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  =============================================================================*/
 
-#define IP_FORWARD "/proc/sys/net/ipv4/ip_forward"
-#define DEBUG 1
+#define IP_FORWARD		"/proc/sys/net/ipv4/ip_forward"
+#define SERVICE_URI		"us.ryanhope.freeTetherD"
+#define DEBUG			1
 
-#include <glib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <dirent.h>
 #include <sys/timeb.h>
 #include <sys/stat.h>
 #include <sys/mount.h>
 
-char *tmpDir;
+#include <glib.h>
+#include <lunaservice.h>
 
-void setup() {
+char *tmpPath;
 
-	int ret = 0, wfd = 0, w = 0;
+bool toggle_ip_forward(LSHandle* lshandle, LSMessage *message, void *ctx) {
 
-	char template[] = "/tmp/freeTether.XXXXXX";
-	tmpDir = mkdtemp(template);
-	if (DEBUG) puts(tmpDir);
+	LSError lserror;
+	LSErrorInit(&lserror);
+
+	FILE *fp;
+	int len = 0, state = 0;
+
+	fp = fopen(tmpPath, "r");
+	if (fp) {
+		state = fgetc(fp);
+		fclose(fp);
+		fp = fopen(tmpPath, "w");
+		if (fp) {
+			switch ((char)state) {
+			case '0': fprintf(fp, "1"); break;
+			case '1': fprintf(fp, "0"); break;
+			}
+			fclose(fp);
+			fp = fopen(tmpPath, "r");
+			state = fgetc(fp);
+			fclose(fp);
+		}
+	}
+
+	char *tmp = 0;
+	len = asprintf(&tmp, "{\"returnValue\":true,\"state\":%c}", (char)state);
+	if (tmp)
+		LSMessageReply(lshandle,message,tmp,&lserror);
+	else
+		LSMessageReply(lshandle,message,"{\"returnValue\":false}",&lserror);
+
+	if (LSErrorIsSet(&lserror)) {
+		LSErrorPrint(&lserror, stderr);
+		LSErrorFree(&lserror);
+	}
+
+	return true;
+
+}
+
+LSMethod methods[] = {
+		{"toggle_ip_forward", toggle_ip_forward},
+		{0,0}
+};
+
+void start_service() {
+
+	LSError lserror;
+	LSErrorInit(&lserror);
+
+	GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+
+	LSPalmService *serviceHandle;
+	LSRegisterPalmService(SERVICE_URI, &serviceHandle, &lserror);
+	LSPalmServiceRegisterCategory(serviceHandle, "/", methods, NULL, NULL, NULL, &lserror);
+	LSGmainAttachPalmService(serviceHandle, loop, &lserror);
+
+	g_main_loop_run(loop);
+
+	if (LSErrorIsSet(&lserror)) {
+		LSErrorPrint(&lserror, stderr);
+		LSErrorFree(&lserror);
+	}
+
+}
+
+char *substr(const char *pstr, int start, int numchars) {
+	char *pnew = malloc(numchars+1);
+	strncpy(pnew, pstr + start, numchars);
+	pnew[numchars] = '\0';
+	return pnew;
+}
+
+int main(int argc, char *argv[]) {
+
+	int ret = 0;
 
 	do {
 		ret = umount(IP_FORWARD);
 	} while (ret==0);
 
+	struct dirent *dp;
+	DIR *tmp = opendir("/tmp");
+	char *sub;
+	while ((dp = readdir(tmp)) != NULL) {
+		sub = substr(dp->d_name,0,11);
+		if (strcmp(sub,"freeTether.")==0) {
+			umount(dp->d_name);
+			rmdir(dp->d_name);
+		}
+		free(sub);
+	}
+	closedir(tmp);
+
+	char template[] = "/tmp/freeTether.XXXXXX";
+	char *tmpDir = strdup(mkdtemp(template));
+
+	ret = asprintf(&tmpPath,"%s/sys/net/ipv4/ip_forward",tmpDir);
+
 	mount("/dev/null", IP_FORWARD, NULL, MS_BIND, NULL);
 	mount("proc", tmpDir, "proc", 0, NULL);
 
-}
-
-void cleanup() {
+	start_service();
 
 	umount(tmpDir);
 	rmdir(tmpDir);
 
 	umount(IP_FORWARD);
 
-}
-
-int main(int argc, char *argv[]) {
-
-	setup();
-
-	GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-	g_main_loop_run(loop);
-
-	cleanup();
 
 	return 0;
 
